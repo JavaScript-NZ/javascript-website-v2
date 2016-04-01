373695202f0d2da166d7fd6e1ab5846c728dbe51
Mike Mackenzie <mike@veb.co.nz>
2016-04-01 23:04:56 +0800

    removed user-accounts routing options

--- app/lib/accounts.js
+++ app/lib/accounts.js
@@ -25,20 +25,3 @@ if (Meteor.isClient) {
     forbidClientAccountCreation: false
   });
 }
-
-AccountsTemplates.configure({
-  forbidClientAccountCreation: false,
-  showForgotPasswordLink: true,
-  overrideLoginErrors: false,
-  enablePasswordChange: true,
-  negativeFeedback: true
-});
-
-AccountsTemplates.configureRoute('changePwd');
-AccountsTemplates.configureRoute('enrollAccount');
-AccountsTemplates.configureRoute('forgotPwd');
-AccountsTemplates.configureRoute('resetPwd');
-AccountsTemplates.configureRoute('signIn');
-AccountsTemplates.configureRoute('signUp');
-AccountsTemplates.configureRoute('verifyEmail');
-
