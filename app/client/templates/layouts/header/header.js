a9245ddc1ed8099ed56f280821d24a0470652673
Mike Mackenzie <mike@veb.co.nz>
2016-04-01 23:05:26 +0800

    header and sidebar

--- app/client/templates/layouts/header/header.js
+++ app/client/templates/layouts/header/header.js
@@ -17,6 +17,22 @@ Template.Header.onCreated(function () {
 });
 
 Template.Header.onRendered(function () {
+  $('.hero').visibility({
+    once: false,
+    continuous: true,
+    onPassing: function (calculations) {
+      var newColor = 'rgba(255, 255, 255, ' + calculations.percentagePassed + ')';
+      $('.masthead')
+        .css('background-color', newColor)
+      ;
+    },
+    onBottomPassed: function (calculations) {
+      console.dir(calculations.percentagePassed);
+      $('.masthead')
+        .css('background-color', 'rgba(255, 255, 255, 1');
+      ;
+    }
+  })
 });
 
 Template.Header.onDestroyed(function () {
