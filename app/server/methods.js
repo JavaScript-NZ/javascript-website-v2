47c12854d9a5208f14044d93d1165c497c523082
Mike Mackenzie <mike@veb.co.nz>
2016-04-06 02:29:43 +0800

    footer/rearrange template

--- app/server/methods.js
+++ app/server/methods.js
@@ -3,7 +3,16 @@
 /*****************************************************************************/
 
 Meteor.methods({
-  'server/method_name': function () {
-    // server method logic
+  'server/method_name': function (doc) {
+    check(doc,  {
+      name: String,
+      email: String,
+      phone: Match.Optional(String),
+      reason: String,
+      message: String
+    });
+    Enquiry.insert(doc,  function () {
+      
+    });
   }
 });
